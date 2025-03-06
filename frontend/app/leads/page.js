cb2efe1ec8fa0fa777379fae4b32e768fa817d7d
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2025-03-07 04:31:16 +0700

    Create Leads page

--- frontend/app/leads/page.js
+++ frontend/app/leads/page.js
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import { Suspense } from "react";
+import LeadList from "../_components/LeadList";
+import Spinner from "../_components/Spinner";
+import LeadForm from "../_components/LeadForm";
+
+export const metadata = {
+  title: "Leads Management",
+  description: "Manage your leads with ease",
+};
 
 export default function Page() {
   return (
     <div>
-      <h1>Start Managing Leads</h1>;<Link href="/leads">Go to Leads</Link>
+      <div className="flex justify-between mb-8">
+        <div className="text-4xl flex gap-2 items-center">
+          <Link href="/">&larr;</Link>
+          <h1 className="text-gray-800 font-medium">Start Managing Leads</h1>
+        </div>
+      </div>
+
+      <LeadForm />
+      <Suspense fallback={<Spinner />}>
+        <LeadList />
+      </Suspense>
     </div>
   );
 }
